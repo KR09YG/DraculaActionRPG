eb9ff8c81d4a2290df7a962e435aee3fa5201300
KRYG <abcdef0509a@gmail.com>
2025-12-09 02:26:12 +0900

    敵アクションの基底クラスの上げ忘れ

--- Assets/Scripts/Enemy/EnemyMove/EnemyActionBase.cs
+++ Assets/Scripts/Enemy/EnemyMove/EnemyActionBase.cs
@@ -8,6 +8,17 @@ public abstract class EnemyActionBase : ScriptableObject
     public float Duration => _duration;
     public float Range => _range;
 
+    /// <summary>
+    /// ‰Šú‰»ˆ—
+    /// </summary>
+    /// <param name="position"></param>
     public abstract void Initialize(Vector2 position);
+    /// <summary>
+    /// Àsˆ—
+    /// </summary>
+    /// <param name="speed"></param>
+    /// <param name="duration"></param>
+    /// <param name="range"></param>
+    /// <param name="transform"></param>
     public abstract void Execute(float speed, float duration, float range, Transform transform);
 }
